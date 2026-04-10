25ef8fd49963201fa90cd5e33c66973c32ec8827
Pradnya Pawar <pradnyapawar4913@gmail.com>
2026-04-10 19:39:11 +0530

    debugging prediction error

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def predict_datapoint():
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
         
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
+        # predict_pipeline = PredictPipeline()
+        # results = predict_pipeline.predict(pred_df)
+        results = [100]
         return render_template('home.html', results=float(results[0]))
  
 import os
