a709b20c4458ce23c7e4a2367baab0a8488f919c
Pradnya Pawar <pradnyapawar4913@gmail.com>
2026-04-10 19:28:33 +0530

    Fixed navigation and deployment

--- app.py
+++ app.py
@@ -38,5 +38,8 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html', results=float(results[0]))
  
-if __name__ =="__main__":
-    app.run(host = "0.0.0.0",debug = True) 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
