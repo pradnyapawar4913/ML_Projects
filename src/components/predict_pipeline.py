d17e2f04646a9133245d350027b9eacba919e932
Pradnya Pawar <pradnyapawar4913@gmail.com>
2026-04-10 21:31:02 +0530

    fixed path issue for render

--- src/components/predict_pipeline.py
+++ src/components/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -7,20 +8,27 @@ class PredictPipeline:
     def __init__(self):
         pass
     
+    import os
+
     def predict(self,features):
-        try:
-            
-           model_path = 'artifacts/model.pkl'
-           preprocessor_path = 'artifacts/preprocessor.pkl'
+         try:
+           base_path = os.path.dirname(os.path.abspath(__file__))
+
+           model_path = os.path.join(base_path, "../../artifacts/model.pkl")
+           preprocessor_path = os.path.join(base_path, "../../artifacts/preprocessor.pkl")
+
            model = load_object(file_path=model_path)
            preprocessor = load_object(file_path=preprocessor_path)
+
            data_scaled = preprocessor.transform(features)
            prediction = model.predict(data_scaled)
+
            return prediction
-       
-        except Exception as e:
+   
+         except Exception as e:
            raise CustomException(e,sys)
-    
+       
+       
 class CustomData:
     def __init__(self,
                  gender:str,
